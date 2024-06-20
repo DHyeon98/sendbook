fd886985b5532dc75771eddedaeb944aeee92296
Lim Dong <ilhai6@naver.com>
2024-06-20 13:31:09 +0900

    pull to refresh 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import { auth } from "@/firebase/firebase";
+import usePullReload from "@/hook/usePullReload";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const pullDownDistance = usePullReload();
+
   const init = async () => {
     // firebase가 쿠키와 토큰을 읽고 백엔드와 소통해서 로그인 여부를 확인하는 동안 기다림.
     await auth.authStateReady();
@@ -11,5 +14,20 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     init();
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div
+        style={{
+          height: `${pullDownDistance}px`,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+        }}
+      >
+        새로고침
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
